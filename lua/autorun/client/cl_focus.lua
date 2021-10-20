4da5a9d60b42d5f863250c6edf45be06ffc4b69b
BuildTools <unconfigured@null.spigotmc.org>
2021-10-20 10:03:02 -0400

    style

--- lua/autorun/client/cl_focus.lua
+++ lua/autorun/client/cl_focus.lua
@@ -17,10 +17,8 @@ end )
 
 hook.Add( "PostDrawTranslucentRenderables", "icon", function()
 	for _, v in ipairs( tabbedOutList ) do -- draws the icon for each tabbed out player
-
 		render.SetColorMaterial() -- Place Holder
 		render.DrawSphere( v:GetPos() + Vector( 0, 0, 75 ), 6,10, 10, Color( 0, 55, 255) ) -- Place Holder
 		render.DrawSphere( v:GetPos() + Vector( 0, 0, 75 ), 4,10, 10, Color( 255, 255, 255) ) -- Place Holder
-
 	end
 end )
