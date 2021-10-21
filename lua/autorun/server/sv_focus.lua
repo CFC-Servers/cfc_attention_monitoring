77019f437ee7c92f56a9d91105866f8459fd178b
BuildTools <unconfigured@null.spigotmc.org>
2021-10-21 00:50:05 -0400

    fixed some minor issues

--- lua/autorun/server/sv_focus.lua
+++ lua/autorun/server/sv_focus.lua
@@ -1,16 +1,14 @@
 local tabbedOutPlys = {}
-local tabbedKeyValue = nil
 
 util.AddNetworkString( "CFC_AttentionMonitor_gameHasFocus" )
 util.AddNetworkString( "CFC_AttentionMonitor_sendData" )
 
 net.Receive( "CFC_AttentionMonitor_gameHasFocus", function( _, pl ) -- Gets the player that tabbed out
 	hasFocus = net.ReadBool()
-	if not hasFocus and not tabbedOutPlys[pl] then
-		table.insert( tabbedOutPlys, pl )
-		tabbedKeyValue = table.KeyFromValue( tabbedOutPlys, pl )
-	elseif tabbedOutPlys then
-		tabbedOutPlys[ tabbedKeyValue ] = nil
+	if not hasFocus and not tabbedOutPlys[ pl ] then -- Checks to see if it needs to add the player
+		tabbedOutPlys[ pl ] = true
+	else
+		tabbedOutPlys[ pl ] = nil
 	end
 
 	net.Start( "CFC_AttentionMonitor_sendData" ) -- Sends the list of players to the client
