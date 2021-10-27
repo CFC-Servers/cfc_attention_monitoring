14e467bff8b487e3339b7f259fb84d1416b238db
code-toby <wahfox8@gmail.com>
2021-10-27 18:21:40 -0400

    added suggestions

--- lua/autorun/server/cfc_attention_monitoring_init.lua
+++ lua/autorun/server/cfc_attention_monitoring_init.lua
@@ -1,4 +1,5 @@
 local tabbedOutPlys = {}
+local lastTabbedOutPlys = {}
 
 util.AddNetworkString( "CFC_AttentionMonitor_GameHasFocus" )
 util.AddNetworkString( "CFC_AttentionMonitor_SendData" )
@@ -13,10 +14,23 @@ local function gameHasFocusCallback( _, ply )
     tabbedOutPlys[ ply ] = not hasFocus or nil
 end
 
+local function compareTables( tab1, tab2 ) -- function made by redox
+    for key, val in pairs( tab1 ) do
+        if val ~= tab2[key] then
+            return false
+        end
+    end
+    return true
+end
+
 timer.Create( "CFC_AttentionMonitor_DataTimmer", 1.2, 0, function()
+    if compareTables( tabbedOutPlys, lastTabbedOutPlys ) then return end
+
     net.Start( "CFC_AttentionMonitor_SendData" ) -- Sends the list of players to the client
         net.WriteTable( tabbedOutPlys )
     net.Broadcast()
+
+    lastTabbedOutPlys = table.Copy(tabbedOutPlys)
 end)
 
 net.Receive( "CFC_AttentionMonitor_GameHasFocus", gameHasFocusCallback )  -- Gets the player that tabbed out
