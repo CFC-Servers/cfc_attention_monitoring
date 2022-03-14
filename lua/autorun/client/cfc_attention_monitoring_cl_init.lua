8aca484521f1faebd8b79bb73fab9b9468acb3ed
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-03-13 18:40:13 -0700

    Use network bools

--- lua/autorun/client/cfc_attention_monitoring_cl_init.lua
+++ lua/autorun/client/cfc_attention_monitoring_cl_init.lua
@@ -1,41 +1,47 @@
-local tabbedOutList = {}
+local IsValid = IsValid
+local ipairs = ipairs
+local player_GetAll = player.GetAll
+
 local isTabbedOut = false
 local icon = Material( "icon16/monitor.png", "3D" )
 
-local function renderables( ply )
-    if not IsValid( ply ) then return end
-    if ply == LocalPlayer() then return end
+local spriteColor = Color( 225, 225, 225, 225 )
+local spriteBoneOffset = Vector( 0, 0, 12 )
+local spriteOffset = Vector( 0, 0, 73 )
 
-        local attachment = ply:GetAttachment( ply:LookupAttachment("eyes") ) -- gets the eye bone of the player
+local function renderTabbedOut( ply )
+    local eyes = ply:LookupAttachment( "eyes" )
+    local attachment = ply:GetAttachment( eyes )
 
-        if attachment then -- checks if it got the bone
-            pos = ply:GetAttachment( ply:LookupAttachment( "eyes" ) ).Pos + Vector( 0, 0, 12 )
-        else
-            pos = ply:GetPos() + Vector( 0, 0, 73 )
-        end
+    if attachment then -- checks if it got the bone
+        pos = attachment.Pos + spriteBoneOffset
+    else
+        pos = ply:GetPos() + spriteOffset
+    end
 
-        render.SetMaterial( icon )
-        render.DrawSprite( pos, 16, 16, Color( 225, 225, 225, 255 ) )
-
+    render.SetMaterial( icon )
+    render.DrawSprite( pos, 16, 16, spriteColor )
 end
 
 timer.Create( "CFC_AttentionMonitor_TabNetTimmer", 0.5, 0, function()
     local hasFocus = system.HasFocus()
-
     if isTabbedOut == hasFocus then return end
 
     net.Start( "CFC_AttentionMonitor_GameHasFocus" )
         net.WriteBool( hasFocus )
     net.SendToServer()
+
     isTabbedOut = hasFocus
 end )
 
-net.Receive( "CFC_AttentionMonitor_SendData", function() -- receives the players that tabbed out
-    tabbedOutList = net.ReadTable()
-end )
-
 hook.Add( "PostPlayerDraw", "CFC_AttentionMonitor_AfkRenderElements", function()
-    for ply in pairs( tabbedOutList ) do -- draws the icon for each tabbed out player
-        renderables( ply )
+    local me = LocalPlayer()
+
+    for _, ply in ipairs( player_GetAll() ) do
+        if ply ~= me and IsValid( ply ) then
+            if ply:GetNW2Bool( "IsTabbedOut" ) then
+                pcall( renderTabbedOut, ply )
+            end
+        end
     end
 end )
